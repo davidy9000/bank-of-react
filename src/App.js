37cae5875be656a606a1f3963fbb0aa737efc49c
davidy9000 <davidy.9000@gmail.com>
2020-01-10 18:21:38 -0500

    passed props to routes

--- src/App.js
+++ src/App.js
@@ -3,15 +3,25 @@ import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Home from './components/Home';
 
 class App extends Component {
-  render() {
-    return (
-        <Router>
-          <Switch>
-            <Route exact path="/" component={Home}/>
-          </Switch>
-        </Router>
-    );
+  constructor() {
+    super();
+
+    this.state = {
+      accountBalance: 14568.27
+    }
   }
+
+  render() {
+      const HomeComponent = () => (<Home accountBalance={this.state.accountBalance}/>);
+    
+      return (
+        <Router>
+          <div>
+            <Route exact path="/" render={HomeComponent}/>
+          </div>
+        </Router>
+      );
+    }
 }
 
 export default App;
