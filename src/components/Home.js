c7ce11c372151a6fd28ae9c9113cab4b91854ac1
davidy9000 <davidy.9000@gmail.com>
2020-01-10 20:24:27 -0500

    add login to home

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,7 +9,8 @@ class Home extends Component {
           <img src="https://letstalkpayments.com/wp-content/uploads/2016/04/Bank.png" alt="bank"/>
           <h1>Bank of React</h1>
 
-          <Link to="/userProfile">User Profile</Link>
+          <div><Link to="/userProfile">User Profile</Link></div>
+          <div><Link to="/LogIn">Log In</Link></div>
 
           <AccountBalance accountBalance={this.props.accountBalance}/>
         </div>
