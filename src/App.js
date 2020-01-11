0063e60ef7442d5c59ba20ba8eac85ea715a429c
davidy9000 <davidy.9000@gmail.com>
2020-01-10 20:16:27 -0500

    add mock login from guide close #4

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import Home from './components/Home';
 import UserProfile from './components/UserProfile';
+import LogIn from './components/LogIn';
 
 class App extends Component {
 
@@ -17,6 +18,12 @@ class App extends Component {
     }
   }
 
+  mockLogIn = (logInInfo) => {
+    const newUser = {...this.state.currentUser}
+    newUser.userName = logInInfo.userName
+    this.setState({currentUser: newUser})
+  }
+
   render() {
 
     const HomeComponent = () => (<Home accountBalance={this.state.accountBalance}/>);
@@ -24,10 +31,13 @@ class App extends Component {
         <UserProfile userName={this.state.currentUser.userName} memberSince={this.state.currentUser.memberSince}  />
     );
 
+    const LogInComponent = () => (<LogIn user={this.state.currentUser} mockLogIn={this.mockLogIn} {...this.props}/>)
+
     return (
         <Router>
           <div>
             <Route exact path="/" render={HomeComponent}/>
+            <Route exact path="/login" render={LogInComponent}/>
             <Route exact path="/userProfile" render={UserProfileComponent}/>
           </div>
         </Router>
