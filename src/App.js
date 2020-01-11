b2e55190d0e763e3827305523e81e4419920e7ea
Eva Yan <eyan0749@gmail.com>
2020-01-11 00:15:09 -0500

    Rearranged styling.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter as Router, Route} from 'react-router-dom';
 import Home from './components/Home';
 import UserProfile from './components/UserProfile';
 import LogIn from './components/LogIn';
+import './App.css';
 
 class App extends Component {
 
@@ -36,6 +37,9 @@ class App extends Component {
     return (
         <Router>
           <div>
+            <div className="App-Header">
+              <h1>Bank of React</h1>
+            </div>
             <Route exact path="/" render={HomeComponent}/>
             <Route exact path="/login" render={LogInComponent}/>
             <Route exact path="/userProfile" render={UserProfileComponent}/>
